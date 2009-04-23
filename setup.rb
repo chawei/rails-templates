51c29636102f5a7de09f60b34c78acfc3290eead
Anonymous <user@gmail.com>
2009-04-23 17:22:19 -0400

    adding setup file

---
+++ setup.rb
@@ -0,0 +1,21 @@
+##
+# Project: App_template
+# Author: Paul Kruger
+# Email: paul@miamiphp.org
+# Company: Speeduneed.com
+# Date Created: April 23 2009
+#This file is for your general github setting and default choices
+##
+
+nickname = 'miamiphp'
+
+github_url = "http://github.com/#{github_nick}/rails-templates/raw/master/"
+
+File.exists?("../config.rb") then
+  include '../config.rb'
+else 
+  load_template github_url + "config.rb"
+end
+
+
+#load_template "http://github.com/grillpanda/rails-templates/raw/master/base.rb"
