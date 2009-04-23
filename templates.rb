b7b730e9db692d9dedca33dda4c7806cbce3c3f3
Anonymous <user@gmail.com>
2009-04-23 18:20:59 -0400

    adding array patches

---
+++ templates.rb
@@ -0,0 +1,13 @@
+#Please Add a file and a description here
+#Base Templates
+templates = [  
+{:name => "Clean Template", :file => "base_template.rb"},  
+{:name => "Plain Rails", :file => "plain_template.rb"}
+]
+
+#Optional Patches
+patches = [
+{:name => "Basic Articles Controller(SEO)", :file => "articles_patch.rb"}
+]
+
+
