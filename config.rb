13e6825a5593531a1a2f0ef77d3489bd8ce54258
Anonymous <user@gmail.com>
2009-04-23 23:31:24 -0400

    updating config file

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 puts "Loading config file"
 
-@project = 'My Project'
-@email = 'paul@miamiphp.org'
-@full_name = 'Paul Kruger'
-@company = 'Speeduneed Inc'
-@company_url = 'http://miamiphp.org'
+project = 'My Project'
+email = 'paul@miamiphp.org'
+full_name = 'Paul Kruger'
+company = 'Speeduneed Inc'
+company_url = 'http://miamiphp.org'
